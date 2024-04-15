3c1338112b13624f00e28e66eaba14a26a0fad71
UnHold (Alex) <alex@hold.re>
2024-04-15 10:16:08 +0200

    added match for ir instructions

--- src/asm.rs
+++ src/asm.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use iced_x86::code_asm::*;
 use iced_x86::Instruction;
 mod lifetime;
@@ -6,17 +8,85 @@ use crate::memory::{self, Executable, Memory, Writeable};
 use crate::ir;
 use crate::parser;
 
+fn get_data_in_register(data: &ir::Data, register: AsmRegister64){
 
+}
 
 #[allow(dead_code)]
 pub fn generate(instructions: &Vec<ir::IrInstruction>, parameters: &parser::Parameters) -> Result<Vec<Instruction>, IcedError> {
     let mut lifetime_checker = lifetime::get_checker(instructions, parameters);
     let mut variable_allocator = var_allocator::VariableAllocator::new(parameters, &mut lifetime_checker);
-    lifetime_checker.print_all();
-    variable_allocator.print_allocated();
-    variable_allocator.allocate("#var_4", 2, &mut lifetime_checker);
-    variable_allocator.print_allocated();
     let mut a = CodeAssembler::new(64)?;
+    let mut labels = HashMap::new();
+
+    for inst in instructions {
+        match inst {
+            ir::IrInstruction::Jump(label) => {
+                let l = a.create_label();
+                a.jmp(l)?;
+                labels.insert(label, l);
+            }
+            ir::IrInstruction::JumpFalse(data, label) => {
+                get_data_in_register(data, rax);
+                a.test(rax, rax)?;
+                let l = a.create_label();
+                a.jz(l)?;
+                labels.insert(label, l);
+            }
+            ir::IrInstruction::Label(label) => {
+
+            }
+            ir::IrInstruction::FunctionCall(res_var, fun_name, args) => {
+
+            }
+            ir::IrInstruction::Addition(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Subtraction(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Multiplication(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Division(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Modulo(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Greater(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::GreaterEquals(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Less(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::LessEquals(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Equals(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::NotEquals(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::LogicAnd(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::LogicOr(res_var, data1, data2) => {
+
+            }
+            ir::IrInstruction::Assignment(res_var, data) => {
+
+            }
+            ir::IrInstruction::PhiNode(res_var, var_name1, var_name2) => {
+
+            }
+        }
+    }
+
 
     Ok(a.take_instructions())
 }
